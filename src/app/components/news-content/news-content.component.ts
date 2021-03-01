fe51037aa97334edc180c1ca73bcfb9f0d4fe044
Joseph <jjoseph_wang@outlook.com>
2021-03-01 22:43:47 +0800

    Add OnDestroy to component

--- src/app/components/news-content/news-content.component.ts
+++ src/app/components/news-content/news-content.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-news-content',
   templateUrl: './news-content.component.html',
   styleUrls: ['./news-content.component.css']
 })
-export class NewsContentComponent implements OnInit {
+export class NewsContentComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngOnDestroy(): void {
+
+  }
 }
--- src/app/components/news-list/news-list.component.ts
+++ src/app/components/news-list/news-list.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-news-list',
   templateUrl: './news-list.component.html',
   styleUrls: ['./news-list.component.css']
 })
-export class NewsListComponent implements OnInit {
+export class NewsListComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+
   }
 
 }
